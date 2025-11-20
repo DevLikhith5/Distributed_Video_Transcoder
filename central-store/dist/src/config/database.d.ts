7c412af7ee221b435d14a63e72fe66041248b9b9
2005LIKHITH <likhithcvsrl@gmail.com>
2025-11-20 19:00:26 +0530

    commit

--- central-store/dist/src/config/database.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "../generated/prisma/client";
-export declare class Database {
-    private static instance;
-    private constructor();
-    static getInstance(): PrismaClient;
-}
-//# sourceMappingURL=database.d.ts.map
--- central-store/dist/src/generated/prisma/models.d.ts
+++
@@ -1,8 +0,0 @@
-export type * from './models/User';
-export type * from './models/Session';
-export type * from './models/Account';
-export type * from './models/Verification';
-export type * from './models/Video';
-export type * from './models/Task';
-export type * from './commonInputTypes';
-//# sourceMappingURL=models.d.ts.map
